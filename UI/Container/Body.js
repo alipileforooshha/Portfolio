426b69831ab6f37e3887c642fe12dec1dc12a8e4
alipileforooshha <alipileforooshha@gmail.com>
2023-04-02 12:45:46 +0430

    add dark mode and minor changes in styling

--- UI/Container/Body.js
+++ UI/Container/Body.js
@@ -1,22 +1,32 @@
-import React, { Fragment } from 'react'
-import {AiFillLinkedin , AiOutlineTwitter} from 'react-icons/ai'
-import {BsInstagram , BsTwitter} from 'react-icons/bs'
+import React, { Fragment } from "react";
+import { AiFillLinkedin, AiOutlineTwitter } from "react-icons/ai";
+import { BsInstagram, BsTwitter } from "react-icons/bs";
 function Body() {
     return (
-        <div className='text-center p-10 mx-auto max-w-xl'>
-            <h2 className='text-4xl py-2 text-teal-500 font-medium'>Ali Pileforooshha</h2>
-            <h3 className=' text-2xl py-2'>Full-Stack Developer</h3>
-            <p className='text-center text-lg leading-8 text-gray-800'>Hi, I'm Ali Pileforooshha a Full-Stack developer working with Laravel and Next.js
-            Over the many years of web developing i have experinced developing many webistes 
-            and have gained alot of experince in both backend and frontend</p>
-            <div className='flex justify-center align-center my-10'>
-                <AiFillLinkedin className=' text-blue-600 text-6xl'/>
-                <AiOutlineTwitter className=' text-blue-300 text-6xl'/>
-                <BsInstagram className=' text-red-600 text-6xl'/>
+        <div className="text-center p-10 mx-auto max-w-5xl">
+            <h2 className="text-4xl py-2 text-teal-500 font-medium">
+                Ali Pileforooshha
+            </h2>
+            <h3 className=" text-2xl py-2">Full-Stack Developer</h3>
+            <p className="text-center text-lg leading-8 text-gray-800 dark:text-slate-300">
+                Hey!, I'm Ali Pileforooshha and I am a seasoned
+                Full-Stack Web Developer. Throughout my years of experience, I
+                have developed a wide range of websites in various industries,
+                and as a result, I have gained extensive expertise in my field.
+                My approach to every project is to comprehend the unique
+                requirements of each client and recommend the most suitable
+                solution for them. I have had the privilege of working on
+                diverse projects such as E-commerce websites, Admin dashboards,
+                Insurance websites, Cryptocurrency platforms, and more.
+            </p>
+            <div className="flex justify-center align-center my-10">
+                <AiFillLinkedin className=" text-blue-600 text-6xl" />
+                <AiOutlineTwitter className=" text-blue-500 text-6xl" />
+                <BsInstagram className=" text-red-600 text-6xl" />
                 {/* <BsTelegram /> */}
             </div>
         </div>
-    )
+    );
 }
 
-export default Body
+export default Body;
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,39 @@
-import Body from '@/UI/Container/Body'
-import Project from '@/UI/Container/Projects/Project'
-import Projects from '@/UI/Container/Projects/Projects'
-import Link from 'next/link'
-import {BsFillMoonStarsFill} from 'react-icons/bs'
+import Body from "@/UI/Container/Body";
+import Project from "@/UI/Container/Projects/Project";
+import Projects from "@/UI/Container/Projects/Projects";
+import Link from "next/link";
+import { useState } from "react";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 function index() {
-  return (
-    <div>
-        <main className="px-10">
-            <section className="min-h-screen text-center mx-auto">
-                <nav className='py-10 mb-10 flex justify-between'>
-                    <h1 className=' text-lg'>Ali Pileforooshha</h1>
-                    <ul className='flex flex-row item-center content-center'>
-                        <li>
-                            <BsFillMoonStarsFill className=' cursor-pointer text-2xl' />
-                        </li>
-                        <li>
-                            <Link href="#" className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'>Resume</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Body/>
-                <Projects />
-            </section>
-        </main>
-    </div>
-  )
+
+    const [darkMode, setDarkMode] = useState(false);
+
+    return (
+        <div className={darkMode ? 'dark' : ""}>
+            <main className="px-10 bg-white dark:bg-emerald-950">
+                <section className="min-h-screen text-center mx-auto">
+                    <nav className="py-10 mb-10 flex justify-between">
+                        <h1 className=" text-lg">Ali Pileforooshha</h1>
+                        <ul className="flex flex-row item-center content-center">
+                            <li>
+                                <BsFillMoonStarsFill className=" cursor-pointer text-2xl" onClick={() =>setDarkMode(!darkMode)}/>
+                            </li>
+                            <li>
+                                <Link
+                                    href="#"
+                                    className=" bg-gradient-to-r from-cyan-500 to-teal-500 
+                                    text-white px-4 py-2 rounded-md ml-8">
+                                    Resume
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Body />
+                    <Projects />
+                </section>
+            </main>
+        </div>
+    );
 }
 
-export default index
+export default index;
